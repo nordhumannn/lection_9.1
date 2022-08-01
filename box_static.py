8bd44c1e7341cdfda536cf8886a3c37ec722aa74
nordhumannn <97041147+nordhumannn@users.noreply.github.com>
2022-08-01 15:33:16 +0300

    Create box_static.py

---
+++ box_static.py
@@ -0,0 +1,22 @@
+class Box:
+
+    def __init__(self, x: int, y: int, z: int):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def volume(self):
+        return self.x * self.y * self.z
+
+    def sum_volume(box_1, box_2):
+        return box_1.volume() + box_2.volume()
+
+    def __str__(self):
+        return f'x = {self.x}, y = {self.y}, z = {self.z}'
+
+
+box_1 = Box(2, 3, 4)
+box_2 = Box(3, 4, 5)
+
+print(box_1.sum_volume(box_2))
+print(Box.sum_volume(box_1, box_2))
